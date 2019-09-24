1d6032de134e9aad4807588fe5d0a65783478642
Stefano Butori <butori@gmail.com>
2019-09-24 16:09:19 -0300

    Visitor is able to see all recipes and details of a recipe

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'recipes#index'
+  get 'recipes', to: 'recipes#index', as: :recipes
+  get 'recipes/:id', to: 'recipes#show', as: :recipe
 end
