c7d797bf1802aa9dce260d79bb75af5f2dfb50af
Stefano Butori <butori@gmail.com>
2019-10-03 10:15:42 -0300

    User is able to see all his recipe lists at once

--- spec/features/user_create_recipe_list_spec.rb
+++ spec/features/user_create_recipe_list_spec.rb
@@ -13,7 +13,22 @@ feature 'User creates recipe list' do
     click_on 'Criar'
 
     expect(page).to have_content('Receitas de Natal')
+    
     recipe_list = RecipeList.last
+
     expect(recipe_list.name).to eq ('Receitas de Natal')
+  end
+
+  scenario 'and can see all his recipe lists at once' do
+    user = User.create(email: 'stefano@revelo.com.br', password: '123456')
+    recipe_list_1 = RecipeList.create!(name: 'Receitas de Natal', user: user)
+    recipe_list_2 = RecipeList.create!(name: 'Receitas de Ano Novo', user: user)
+
+    login_as(user, scope: :user)
+    visit root_path
+    click_on 'Minhas listas de receitas'
+
+    expect(page).to have_content('Receitas de Natal')
+    expect(page).to have_content('Receitas de Ano Novo')
   end
 end
