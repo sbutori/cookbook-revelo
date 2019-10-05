fea3cc3f7c8fa4f260791e8dd4a31f7620cb2d21
Stefano Tosi Butori <butori@gmail.com>
2019-10-05 16:58:07 -0300

    Change search route and add admin panel route

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,14 @@ Rails.application.routes.draw do
     resources :recipe_lists, only: [] do
       post 'add_recipe', on: :member
     end
-    # post '/:recipe_list_id/add_recipe_to_list', to: 'recipe_lists#add_recipe'
+    # get 'search', to: 'recipes#search', as: 'recipe_search'
+    collection do
+      get 'search'
+    end
   end
   resources :recipe_types, only: %i[index show new create]
   resources :cuisines, only: %i[index show new create]
   resources :recipe_lists, only: %i[index show new create]
   get 'my-recipes', to: 'recipes#my_recipes', as: 'my_recipes'
-  get 'search', to: 'recipes#search', as: 'recipe_search'
-  # get 'minhas-receitas', to: 'user#recipes', as: 'my-recipes'
-  # TRANSFORMAR ROTA EM NESTED (MEMBER) E EM POST
-  # get 'add_recipe/:id/:recipe_id', to: 'recipe_lists#add_recipe', as: 'add_recipe'
+  get 'admin-panel', to: 'user#admin_panel', as: 'admin_panel'
 end
